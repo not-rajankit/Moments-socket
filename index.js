3cc2d7f8fb55d63b419ed9415ebdbfa6a6e1b3a6
not-rajankit <work.rajankit@gmail.com>
2024-08-24 18:29:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const io = require("socket.io")(8900, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://moments-frontend-sepia.vercel.app",
   },
 });
 let users = [];
